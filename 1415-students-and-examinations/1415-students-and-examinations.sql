a4d11e2de79a4ccf97b1ce52d2e5b37c64b2f10d
YuvarajKate <84085324+YuvarajKate@users.noreply.github.com>
2025-10-06 22:27:46 +0530

    Time: 989 ms (39.29%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select s.student_id, s.student_name, sb.subject_name, ifnull(count(e.student_id),0) attended_exams
+select s.student_id, s.student_name, sb.subject_name, ifnull(count(e.student_id),0) "attended_exams"
 from students s cross join subjects sb  left join examinations e
 on sb.subject_name=e.subject_name and s.student_id=e.student_id
 group by s.student_id, sb.subject_name
