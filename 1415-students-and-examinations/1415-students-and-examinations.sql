1047561c9d29d57e16b78ffdc0e5cf7474cf4368
YuvarajKate <84085324+YuvarajKate@users.noreply.github.com>
2025-10-06 22:28:00 +0530

    Time: 1012 ms (34.9%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -3,4 +3,4 @@ select s.student_id, s.student_name, sb.subject_name, ifnull(count(e.student_id)
 from students s cross join subjects sb  left join examinations e
 on sb.subject_name=e.subject_name and s.student_id=e.student_id
 group by s.student_id, sb.subject_name
-order by s.student_id, sb.subject_name;
+order by s.student_id, sb.subject_name
